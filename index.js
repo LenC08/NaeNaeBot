4f1ad771296cf2c20dbe71612646d65cc0d56297
LenC08 <79581412+LenC08@users.noreply.github.com>
2023-06-27 20:16:21 +0200

    fix

--- index.js
+++ index.js
@@ -100,8 +100,8 @@ client.on("ready", () => {
     // run every 10 seconds
     setInterval(() => {
         // generate random number between 1 and list length.
-        const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
-        const newActivity = activities[randomIndex - 1];
+        const randomIndex = Math.floor(Math.random() * (activities.length));
+        const newActivity = activities[randomIndex];
 
         client.user.setActivity(newActivity);
     }, 10000);
